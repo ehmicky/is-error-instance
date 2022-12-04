c50fd1d42883470331b155704fa5413e320334a1
ehmicky <ehmicky@gmail.com>
2022-12-04 19:34:30 +0100

    Improve documentation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+# 1.4.0
+
+## Features
+
+- Improve documentation
+
 # 1.3.0
 
 ## Features
--- src/main.d.ts
+++ src/main.d.ts
@@ -1,8 +1,34 @@
 /**
+ * Check if a value is an `Error` instance.
+ *
+ * This is like `value instanceof Error` except it works across realms, such as
+ * iframes or Node.js [`vm`](https://nodejs.org/api/vm.html).
  *
  * @example
  * ```js
+ * isErrorInstance(new Error('')) // true
+ * isErrorInstance('') // false
+ *
+ * const CrossRealmError = vm.runInNewContext('Error')
+ * isErrorInstance(new CrossRealmError('')) // true
+ *
+ * isErrorInstance(new TypeError('')) // true
+ * isErrorInstance(new AnyOtherError('')) // true
+ *
+ * isErrorInstance(new DOMException('')) // true
+ * isErrorInstance(new DOMError('')) // true
+ *
+ * isErrorInstance(new Proxy(new Error(''), {})) // true
+ * isErrorInstance(
+ *   new Proxy(new Error(''), {
+ *     getPrototypeOf() {
+ *       throw new Error('')
+ *     },
+ *   }),
+ * ) // false
  * ```
  */
+// eslint-disable-next-line import/export
 export default function isErrorInstance(value: Error): true
+// eslint-disable-next-line import/export
 export default function isErrorInstance(value: any): false
